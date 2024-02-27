96e8759ef9ef131826522236c0f5a1ccfee17f79
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-26 21:18:40 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -7,7 +7,7 @@ from PIL import Image
 import os
 
 # Model Parameters
-latent_dim = 16  # Example latent space dimension
+latent_dim = 32  # Example latent space dimension
 LATENT_DIM = latent_dim
 
 class VariationalAutoencoder(nn.Module):
